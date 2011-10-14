552dff8fab35a3bbb78fe4bf4ffa7a7dce975e90
Jeremy Sharpe <jeremy.adamson.sharpe@gmail.com>
2011-10-13 20:37:52 -0700

    avoid case sensitivity in font file name by matching the file name with the name in code (army.ttf). This was not a problem for me, but apparently caused issues elsewhere. Thanks to Equal (https://github.com/Equal).

--- font.py
+++ font.py
@@ -9,6 +9,6 @@ class FontManager:
     k = (text, size, color[:4])
     # rendering text is reeeeally slow in pygame, so cache it
     if not k in self.cache:
-      font = pygame.font.Font(os.path.join(constants.DATA_DIR, "Army.ttf"), size)
+      font = pygame.font.Font(os.path.join(constants.DATA_DIR, "army.ttf"), size)
       self.cache[k] = font.render(text, 1, color)
     return self.cache[k]
